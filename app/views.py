65b747ccd97a0d916b19db76e00b582b6cc4a342
Joel Henry <joel.henry.023@gmail.com>
2022-04-10 12:21:14 -0500

    Views Update

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file creates your application.
 """
 
+from crypt import methods
 from app import app
 from flask import render_template, request, jsonify, send_file
 import os
@@ -17,6 +18,66 @@ import os
 @app.route('/')
 def index():
     return jsonify(message="This is the beginning of our API")
+
+
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    return jsonify('Register')
+
+
+
+@app.route('/api/auth/login', methods=['POST'])
+def login():
+    return jsonify('Login')
+
+
+
+@app.route('/api/auth/logout', methods=['POST'])
+def logout():
+    return jsonify('logout')
+
+
+@app.route('/api/cars', methods=['GET', 'POST'])
+def cars():
+    if (request.method == 'POST'):
+        return jsonify('Add Cars')
+    return jsonify('Get Cars')
+
+
+
+@app.route('/api/cars/{car_id}', methods=['GET'])
+def vehicle(car_id):
+    return jsonify('Vehicle')
+
+
+
+@app.route('/api/cars/{car_id}/favourite', methods=['POST'])
+def addVehicleToFavourite():
+    return jsonify('Add Vehicle to Favourite')
+
+
+
+@app.route('/api/search', methods=['GET'])
+def searchInventory():
+    return jsonify('Search Inventory')
+
+
+@app.route('/ap/users/{user_id}', methods=['GET'])
+def getUserData(user_id):
+    return jsonify('Get User')
+
+
+
+@app.route('/api/users/{user_id}/favourites', methods=['GET'])
+def getCarsUsersLike(useer_id):
+    return jsonify('Get Cars user likes')
+
+
+
+
+
+
 
 
 ###
