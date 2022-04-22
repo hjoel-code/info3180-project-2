616efc8075fbebdf39c45a84d79a841b6ff01021
Sabrina Graham <techiepotato3004@gmail.com>
2022-04-21 20:26:11 -0400

    Car Details Css update

--- src/components/Explore/CarDetails.vue
+++ src/components/Explore/CarDetails.vue
@@ -22,32 +22,62 @@
             <div class="d-flex flex-row pt-3">
                 <div class="d-flex flex-column">
                     <div class="d-flex flex-row ">
-                        <div class="text-muted font-weight-bold px-3 ">Color</div>
+                        <div class="text-muted font-weight-bold  ">Color:</div>
                         <div class="font-weight-bold px-3 ">{{ cars.colour }}</div>
                     </div>
                     <br>
                     <div class="d-flex flex-row">
-                        <div class="text-muted font-weight-bold px-3">Price</div>
-                        <div class="font-weight-bold px-3">{{ formatPrice(cars.price)  }}</div>
+                        <div class="text-muted font-weight-bold ">Price:</div>
+                        <div class="font-weight-bold px-3">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="feather feather-tag"
+                            >
+                            <path
+                                d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z"
+                            ></path>
+                             <line x1="7" y1="7" x2="7.01" y2="7"></line>
+                            </svg>
+                            
+                            {{ formatPrice(cars.price)  }}
+                        </div>
                     </div>
                 </div>
                 <div class="p-4"></div>
-                <div class="d-flex flex-column ">
+                <div class="d-flex flex-column justify-column-end">
                     <div class="d-flex flex-row">
-                        <div class="text-muted font-weight-bold px-3">Body Type</div>
+                        <div class="text-muted font-weight-bold ">Body Type:</div>
                         <div class="font-weight-bold px-3">{{ cars.car_type }}</div>
                     </div>
                     <br>
                     <div class="d-flex flex-row">
-                        <div class="text-muted font-weight-bold px-3">Transmission</div>
+                        <div class="text-muted font-weight-bold ">Transmission:</div>
                         <div class="font-weight-bold px-3">{{ cars.trans }}</div>
                     </div> 
                 </div>
             </div>
-            <div class="pt-4">
-                <a href="{{url_for('addVehicleToFavourite()')}}" class="btn btn-secondary btn-success mt-4">
+            <br><br><br><br>
+            <div class="d-flex row">
+
+                <div class="">
+                <a href="" class="btn btn-secondary btn-success mt-4">
                     <small>Email Owner</small>
                 </a>
+                </div>
+                <div class=" d-flex justify-content-end w-100">
+               
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16">
+                <g id="_01_align_center" data-name="01 align center"><path d="M17.5.917a6.4,6.4,0,0,0-5.5,3.3A6.4,6.4,0,0,0,6.5.917,6.8,6.8,0,0,0,0,7.967c0,6.775,10.956,14.6,11.422,14.932l.578.409.578-.409C13.044,22.569,24,14.742,24,7.967A6.8,6.8,0,0,0,17.5.917ZM12,20.846c-3.253-2.43-10-8.4-10-12.879a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,7.967h2a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,7.967C22,12.448,15.253,18.416,12,20.846Z"/></g>
+                </svg>
+                </div>
             </div>
         </div>
 
